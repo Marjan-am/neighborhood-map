f9391ce2c6276b773ef22c04b2dabb6842c3bd2b
marjan <marjan.arod@gmail.com>
2019-01-12 13:32:37 -0800

    Updates

---
+++ precache-manifest.4e74b7e7ff11f5179f634c45b7a09db8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1627007f1dec8dd542c",
+    "url": "/neighborhood-map/static/js/runtime~main.f1627007.js"
+  },
+  {
+    "revision": "a43a6ea8990737b379b9",
+    "url": "/neighborhood-map/static/js/main.a43a6ea8.chunk.js"
+  },
+  {
+    "revision": "454c06b1e5c3c7d4a87f",
+    "url": "/neighborhood-map/static/js/1.454c06b1.chunk.js"
+  },
+  {
+    "revision": "a43a6ea8990737b379b9",
+    "url": "/neighborhood-map/static/css/main.be431db4.chunk.css"
+  },
+  {
+    "revision": "89b3c089fd917251175cfbdde7d55fc4",
+    "url": "/neighborhood-map/index.html"
+  }
+];
